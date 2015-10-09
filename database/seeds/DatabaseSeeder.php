2172fe5c89eec926c421f4cd14313ef0c91576f3
Michael Shelton <the.mikex@gmail.com>
2015-10-09 21:58:42 +0000

    commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,19 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+                     DB::table('posts')->insert([
+            'title' => 'ScotSki-line-one',
+            'content' => "Welcome to ScotSki, Scotland's premiere skiing and snowboarding holiday provider. We offer a range of exciting, inclusive and fun packed holidays to the coolest destinations Scotland has to offer.",
+        ]);
+                     DB::table('posts')->insert([
+            'title' => 'ScotSki-line-two',
+            'content' => "Whether you are an experienced snowboarder, improving skier or a family looking to have fun in some of the most picturesque and breathtaking mountain ranges in Scotland, ScotSki has something for you.",
+        ]);
+                    DB::table('posts')->insert([
+            'title' => 'ScotSki-line-three',
+            'content' => "So browse our site and give us a call to book the experience of a lifetime. We promise you will have the time of your life.",
+        ]);
+
         // $this->call(UserTableSeeder::class);
 
         Model::reguard();
