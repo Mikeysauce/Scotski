20e523455bfbae5d98b75983d8e0a21d81d56122
Michael Shelton <the.mikex@gmail.com>
2015-10-12 14:30:06 +0000

    commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,6 +23,7 @@
 	{!! Html::style('/css/style.css') !!}
 </head>
 <body>
+<div class="transwrap" style="display:none;"><div id="google_translate_element"></div></div>
 @section('header')
 @include('layouts.partials.header')
 @show
@@ -53,6 +54,41 @@
  <!-- scripts -->
 {!! Html::script('//code.jquery.com/jquery-2.1.4.min.js') !!}
 {!! Html::script('//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js') !!}
+
+<script type="text/javascript">
+  function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', autoDisplay: false}, 'google_translate_element'); //remove the layout
+  }
+</script>
+<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" type="text/javascript"></script>
+
+
+<script type="text/javascript">
+    function triggerHtmlEvent(element, eventName) {
+var event;
+if(document.createEvent) {
+    event = document.createEvent('HTMLEvents');
+    event.initEvent(eventName, true, true);
+    element.dispatchEvent(event);
+} else {
+    event = document.createEventObject();
+    event.eventType = eventName;
+    element.fireEvent('on' + event.eventType, event);
+}
+}
+        $('.translation-links a').click(function(e) {
+  e.preventDefault();
+  var lang = $(this).data('lang');
+  $('#google_translate_element select option').each(function(){
+    if($(this).text().indexOf(lang) > -1) {
+        $(this).parent().val($(this).val());
+        var container = document.getElementById('google_translate_element');
+        var select = container.getElementsByTagName('select')[0];
+        triggerHtmlEvent(select, 'change');
+    }
+});
+});
+        </script>
 @yield('scripts')
 </body>
 </html>
--- resources/views/layouts/partials/navbar-loggedin.blade.php
+++ resources/views/layouts/partials/navbar-loggedin.blade.php
@@ -1,13 +1,13 @@
 <div class="container">
     <nav id="custom-bootstrap-menu" class="navbar navbar-default navbar-static-top" role="navigation">
         <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        </button>
-        <span class="navbar-brand">ScotSki</span>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <span class="navbar-brand">ScotSki</span>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
@@ -17,22 +17,21 @@
                 <li class="{{ Request::is('facilities') ? 'active' : '' }}"><a href="{{ URL::to('facilities') }}">Facilities</a></li>
                 <li class="{{ Request::segment(1) === 'penguinclub' ? 'active' : null }}"><a href="{{ URL::to('penguinclub') }}">Penguin Club</a></li>
               </ul>
-               <ul class="nav navbar-nav navbar-right translation-links" style="margin-right: 0px;">
+            <ul class="nav navbar-nav navbar-right translation-links" style="margin-right: 0px;">
                <li><a href="#" class="spanish" data-lang="Spanish"><img src="{{asset('flags/es.gif')}}"></a></li>
                <li><a href="#" class="english" data-lang="English"><img src="{{asset('flags/gb.gif')}}"></a></li>
                <li> <a href="#" class="french" data-lang="French"><img src="{{asset('flags/fr.gif')}}"></a></li>
                 <li> <a href="#" class="german" data-lang="German"><img src="{{asset('flags/de.gif')}}"></a></li>
-               </ul>
             </ul>
-      <ul class="nav navbar-nav navbar-right" style="margin-right: 0px;">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin<span class="caret"></span></a>
-            <ul class="dropdown-menu">
-                 <li class="{{ Request::is('posts') ? 'active' : '' }}"><a href="{{ URL::to('posts') }}">Update Homepage</a></li>
-                 <li class="{{ Request::is('auth/logout') ? 'active' : '' }}"><a href="{{ URL::to('auth/logout') }}">Logout</a></li>
+             <ul class="nav navbar-nav navbar-right" style="margin-right: 0px;">
+                 <li class="dropdown">
+                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin<span class="caret"></span></a>
+                     <ul class="dropdown-menu">
+                         <li class="{{ Request::is('posts') ? 'active' : '' }}"><a href="{{ URL::to('posts') }}">Update Homepage</a></li>
+                         <li class="{{ Request::is('auth/logout') ? 'active' : '' }}"><a href="{{ URL::to('auth/logout') }}">Logout</a></li>
+                     </ul>
+                 </li>
             </ul>
-        </li>
-      </ul>
-              </div>
-    </nav>
+    </div>
+ </nav>
 </div>
