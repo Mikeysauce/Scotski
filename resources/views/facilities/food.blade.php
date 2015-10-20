8d6297f7bbea6bf759d67afdd7b1db9f59626cc5
Michael Shelton <the.mikex@gmail.com>
2015-10-20 17:01:05 +0000

    gogo

--- resources/views/facilities/food.blade.php
+++ resources/views/facilities/food.blade.php
@@ -1,15 +1,13 @@
 @extends('layouts.master')
 @section('content')
-    @if(Session::has('message'))
-    <div class="alert alert-info">
-      {{Session::get('message')}}
-    </div>
-@endif
- <div class="jumbotron nicer">
- <h2 class="special2">Food & Drinks</h2>
-    <hr class="style-two" style="
-    width: 8%;
-">
-
+<div class="jumbotron nicer">
+    <h2 class="special2">Food & Drinks</h2>
+    <hr class="style-two" style="width: 8%;">
+    <p>
+    Each resort has a lovely café restaurant to warm your feet after a day on the slopes and also a booking only À la carte restaurant which serves high quality local produce.
+    </p>
+    <p>
+    For a little teaser of what we have on offer, check out the image gallery in the facilities section.
+    </p>
 </div>
 @stop
--- resources/views/facilities/gym.blade.php
+++ resources/views/facilities/gym.blade.php
@@ -1,15 +1,10 @@
 @extends('layouts.master')
 @section('content')
-    @if(Session::has('message'))
-    <div class="alert alert-info">
-      {{Session::get('message')}}
-    </div>
-@endif
- <div class="jumbotron nicer">
- <h2 class="special2">Gym</h2>
-    <hr class="style-two" style="
-    width: 8%;
-">
-
+<div class="jumbotron nicer">
+    <h2 class="special2">Gym</h2>
+    <hr class="style-two" style="width: 8%;">
+    <p>
+    Our resorts have a 24 hour Gym that is fully equipped with the latest fitness equipment as well as weights and yoga mats.
+    </p>
 </div>
 @stop
