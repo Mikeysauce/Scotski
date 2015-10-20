5468390e3af725ca1c5377c55fbe9c47629d4cfd
Michael Shelton <the.mikex@gmail.com>
2015-10-20 17:03:59 +0000

    gogo

--- resources/views/facilities/spa.blade.php
+++ resources/views/facilities/spa.blade.php
@@ -1,15 +1,13 @@
 @extends('layouts.master')
 @section('content')
-    @if(Session::has('message'))
-    <div class="alert alert-info">
-      {{Session::get('message')}}
-    </div>
-@endif
- <div class="jumbotron nicer">
- <h2 class="special2">Spa & Sauna</h2>
-    <hr class="style-two" style="
-    width: 8%;
-">
-
+<div class="jumbotron nicer">
+    <h2 class="special2">Spa & Sauna</h2>
+    <hr class="style-two" style="width: 8%;">
+    <p>
+    Relax and enjoy a nice warm Sauna and a full body massage in our luxurious Spa, perfect for the evening time!
+    </p>
+    <p>
+    For a little teaser of what we have on offer, check out the image gallery in the facilities section.
+    </p>
 </div>
 @stop
