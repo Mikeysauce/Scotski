c1fdf0c506e27f18a38578615924171d04851105
Michael Shelton <the.mikex@gmail.com>
2015-10-23 10:51:29 +0000

    hoho

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>ScotSki - @yield('title')</title>
 
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width">
 	<meta name="google" content="notranslate" />
   @yield('meta')
